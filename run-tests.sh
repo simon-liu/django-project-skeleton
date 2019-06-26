7d1ade22d0f93af954d66a8c60e185196920fd8e
liuzenglu <zenglu.liu@gmail.com>
2019-06-26 13:43:44 +0800

    fix

--- run-tests.sh
+++ run-tests.sh
@@ -2,12 +2,11 @@
 
 flake8 --max-line-length=200 {{ project_name|lower }}
 
-flake8 --max-line-length=100 apps \
-    | grep -v '\/migrations\/.*E501 line too long'
+flake8 --max-line-length=100 apps --exclude apps/*/migrations/*.py
 
 export DJANGO_SETTINGS_MODULE={{ project_name|lower }}.settings.ut
 
-if [ -z "$1" ]
+if [[ -z "$1" ]]
 then
     coverage run --source='.' manage.py test \
         apps.main.tests_main
